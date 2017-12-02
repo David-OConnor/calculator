3fa1b985215127ef7c8b814fdb804faee7dc28f0
David O'Connor <david.alan.oconnor@gmail.com>
2017-12-02 09:05:07 +0000

    Tweaks to config files; added asquare function to make square arrays

--- num.py
+++ num.py
@@ -7,7 +7,8 @@ from scipy.constants import *
 Å = angstrom # from scipy.constants
 
 from numpy import array, linspace, arange, zeros, ones, diff, real, imag, \
-    eye, sin, cos, tan, arcsin, arccos, arctan, arctan2, sinh, cosh, tanh, \
+    eye, sin, cos, tan, arcsin, arccos, arctan, arctan2,  arcsin as asin, \
+    arccos as acos, arctan as atan, sinh, cosh, tanh, \
     log, sqrt, \
     e, pi, exp
 
@@ -23,3 +24,12 @@ coth = lambda x: 1 / tanh(x)
 tau = 2 * pi
 π, τ = pi, tau
 i = complex(0, 1)
+
+
+# Temp? OR should I keep it?
+
+def asquare(*items):
+    """Helper for making square arrays."""
+    side_len = sqrt(len(items))
+    assert not side_len % 1, "Array must be square." # Array numbers must form a square.
+    return array(items).reshape(int(side_len), int(side_len))
